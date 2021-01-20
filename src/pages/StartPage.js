651c024ef5b5e608815265a314f6d6faa32c8363
Elin Boström <ebostrom@live.se>
2021-01-20 21:21:22 +0100

    Render joblist if state is updated

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,18 +1,23 @@
 import React, { useContext, useState, useEffect } from 'react'
+import ButtonPreviousSearchTerm from '../components/ButtonPreviousSearchTerm';
+import JobList from '../components/JobList';
 import SearchForm from '../components/SearchForm'
 import { JobContext } from '../contexts/JobContextProvider';
 
 export default function StartPage() {
   const { previousSearchItems } = useContext(JobContext);
 
-  useEffect(() => {
-    console.log(previousSearchItems);
-  }, [previousSearchItems])
+  // useEffect(() => {
+  //   emptyList = previousSearchItems.jobs ?? null;
+  // }, [previousSearchItems])
 
   return (
     <main>
       <h1>GitHub Job Listing</h1>
       <SearchForm />
+      {previousSearchItems && <ButtonPreviousSearchTerm previousSearchItems={previousSearchItems} />}
+      {previousSearchItems && <JobList previousSearchItems={previousSearchItems} />}
+      {previousSearchItems && Array.isArray(previousSearchItems.jobs) && previousSearchItems.jobs.length <= 0 && <h2>No jobs found</h2>}
     </main>
   )
 }
