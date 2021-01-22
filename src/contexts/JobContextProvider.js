46f05d74b14b352b3d4e5c31f045e0342d907afa
Elin Boström <ebostrom@live.se>
2021-01-22 11:27:52 +0100

    Add dangerouslySetInnerHtml and jobObj function

--- src/contexts/JobContextProvider.js
+++ src/contexts/JobContextProvider.js
@@ -4,10 +4,27 @@ export const JobContext = createContext({});
 
 export default function JobContextProvider({ children }) {
   const API_URL = "https://us-central1-wands-2017.cloudfunctions.net/githubjobs?description=";
-  const [jobList, setJobList] = useState(null);
+  const [jobList, setJobList] = useState([]);
+  const [previousSearchItems, setPreviousSearchItems] = useState([]);
+
+  const createMarkup = (data) => {
+    return { __html: data };
+  }
+
+  const jobObj = (searchTerm, data) => {
+    return { term: searchTerm, jobs: data }
+  }
 
   return (
-    <JobContext.Provider value={{ API_URL, jobList, setJobList }}>
+    <JobContext.Provider value={{
+      API_URL,
+      jobList,
+      setJobList,
+      previousSearchItems,
+      setPreviousSearchItems,
+      createMarkup,
+      jobObj
+    }}>
       {children}
     </JobContext.Provider>
   )
