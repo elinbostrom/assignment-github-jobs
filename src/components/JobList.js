29c9b4c090bfb358b840c280ca0fb97f53b95c6d
Elin Boström <ebostrom@live.se>
2021-01-22 11:26:23 +0100

    Update JobList so it renders correctly

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -2,13 +2,13 @@ import React from 'react'
 import JobItem from './JobItem';
 
 export default function JobList({ jobList }) {
-  const jobs = Object.entries(jobList.jobs);
+  const jobs = jobList.jobs;
 
   return (
     <ul>
       {jobs && Array.isArray(jobs) && jobs.map(job => {
         return (
-          <JobItem key={job[1].id} data={job[1]} />
+          <JobItem key={job.id} data={job} />
         )
       })}
     </ul>
